7e0e7d4cd40a371c0bdb297217005fb5bc358601
Tom Garrett <tomgarrett@Toms-iMac.local>
2023-08-16 09:05:51 -0500

    new edit

--- first.py
+++ first.py
@@ -1,8 +1,5 @@
 def hey(s_name):
-    return f"Hello There {s_name}"
+    return f"Hello there {s_name}"
 
 def next(s_name):
-    return f"hey hey hey {s_name}"
-
-def next(s_name):
-    return f"another one {s_name}"
+    return f"How are you {s_name} ?"
